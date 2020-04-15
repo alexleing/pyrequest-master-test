6d1e3353e26b8ac89faa36e93dbb28e5e13a699b
alexleing <lechunxiaing@163.com>
2020-04-15 16:09:45 +0800

    优化数据SQL语句

--- db_fixture/create_data.py
+++ db_fixture/create_data.py
@@ -1,15 +1,19 @@
 f = open("data_test.sql", 'w')
 
 
-for i in range(1, 1000):
-    str_i = str(i)
-    real_name = "alex" + str_i
-    phone = 1780000000 + i
-    email = "alex" + str_i +"@mail.com"
-    sql = 'INSERT INTO sign_guest (id,real_name, phone, email, sign, event_id) VALUES ' \
-          '('+str_i+',"'+real_name+'", '+str(phone)+', "'+email+'", 0, 1);'
-    f.write(sql)
-    f.write("\n")
+try:
+    for i in range(1, 1000):
+        str_i = str(i)
+        real_name = "alex" + str_i
+        phone = 1780000000 + i
+        email = "alex" + str_i +"@mail.com"
+        sql = 'INSERT INTO sign_guest (id,real_name, phone, email, sign, event_id) VALUES ' \
+              '('+str_i+',"'+real_name+'", '+str(phone)+', "'+email+'", 0, 1);'
+        f.write(sql)
+        f.write("\n")
+
+except:
+    print("数据新建失败。")
 
 
 f.close()
