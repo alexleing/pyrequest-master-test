bab4ec441f4787ab01b971d7a912c78d6cd8fb1d
alexleing <lechunxiaing@163.com>
2020-04-15 15:31:01 +0800

    修改数据库表的结构 给数据库表新增ID字段属性

--- db_fixture/mysql_db.py
+++ db_fixture/mysql_db.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     table_name = "sign_event"
     data = {'id':1,'name':'红米','`limit`':2000,'status':1,'address':'北京会展中心','start_time':'2016-08-20 00:25:42'}
     table_name2 = "sign_guest"
-    data2 = {'realname':'alen','phone':12312341234,'email':'alen@mail.com','sign':0,'event_id':1}
+    data2 = {'id':1,'realname':'alen','phone':12312341234,'email':'alen@mail.com','sign':0,'event_id':1}
 
     db.clear(table_name)
     db.insert(table_name, data)
